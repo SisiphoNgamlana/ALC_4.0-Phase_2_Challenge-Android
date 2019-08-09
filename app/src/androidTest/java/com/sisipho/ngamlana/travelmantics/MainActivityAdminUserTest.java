643f323adbabb0a036f5551fa1b1196a9893344c
SisiphoNgamlana <watziznaim@gmail.com>
2019-08-09 14:09:34 +0200

    Apply formatting to the new tests and fix failing tests

--- app/src/androidTest/java/com/sisipho/ngamlana/travelmantics/MainActivityAdminUserTest.java
+++ app/src/androidTest/java/com/sisipho/ngamlana/travelmantics/MainActivityAdminUserTest.java
@@ -1,14 +1,19 @@
 package com.sisipho.ngamlana.travelmantics;
 
 import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.List;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
 import static androidx.test.espresso.contrib.DrawerMatchers.isOpen;
@@ -17,11 +22,18 @@ import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.hamcrest.Matchers.not;
 
 public class MainActivityAdminUserTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    private static List<TravelDeal> travelDealList;
+
+    @Before
+    public void setup() {
+        travelDealList = FireBaseUtil.travelDealList;
+    }
 
     @Test
     public void onResume() {
@@ -29,11 +41,20 @@ public class MainActivityAdminUserTest {
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
         onView(withId(R.id.nav_view)).check(matches(isDisplayed()));
         onView(withId(R.id.drawer_layout)).check(matches(isOpen()));
-        onView(withId(R.id.nav_send)).check(matches(isEnabled()));
+        onView(withText(R.string.menu_send)).check(matches(isEnabled()));
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.close());
-        pressBack();
         onView(withId(R.id.drawer_layout)).check(matches(isClosed()));
 
+        for (int index = 0; index < travelDealList.size(); index++) {
+            TravelDeal travelDeal = travelDealList.get(index);
+
+            onView(withId(R.id.recyclerView_trade_deals)).perform(RecyclerViewActions.actionOnItemAtPosition(index, click()));
+            onView(withId(R.id.textView_title)).check(matches(withText(travelDeal.getTitle()))).check(matches(not(isEnabled())));
+            onView(withId(R.id.textView_description)).check(matches(withText(travelDeal.getDescription()))).check(matches(not(isEnabled())));
+            onView(withId(R.id.textView_price)).check(matches(withText(travelDeal.getPrice()))).check(matches(not(isEnabled())));
+
+            pressBack();
+        }
     }
 
     @Test
@@ -42,36 +63,3 @@ public class MainActivityAdminUserTest {
         onView(withText(R.string.text_logout)).check(matches(isDisplayed()));
     }
 }
-
-//    @Rule
-//    public ActivityTestRule<ListActivity> listActivityTestRule = new ActivityTestRule(ListActivity.class);
-//    private static List<TravelDeal> travelDealList;
-//
-//    @Before
-//    public void setup() {
-//        travelDealList = FireBaseUtil.travelDealList;
-//    }
-//
-//    @Test
-//    public void onResume() {
-//
-//        for (int index = 0; index < travelDealList.size(); index++) {
-//            TravelDeal travelDeal = travelDealList.get(index);
-//
-//            onView(withId(R.id.recyclerView_trade_deals)).perform(RecyclerViewActions.actionOnItemAtPosition(index, click()));
-//            onView(withId(R.id.textView_item_title)).check(matches(withText(travelDeal.getTitle())));
-//            onView(withId(R.id.textView_item_title)).check(matches(not(isEnabled())));
-//            onView(withId(R.id.textView_item_description)).check(matches(withText(travelDeal.getDescription())));
-//            onView(withId(R.id.textView_item_description)).check(matches(not(isEnabled())));
-//            onView(withId(R.id.textView_item_price)).check(matches(withText(travelDeal.getPrice())));
-//            onView(withId(R.id.textView_item_price)).check(matches(not(isEnabled())));
-//
-//            pressBack();
-//        }
-//    }
-//
-//    @Test
-//    public void onCreateOptionsMenu() {
-//        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-//        onView(withText(R.string.text_logout)).check(matches(isDisplayed()));
-//    }
